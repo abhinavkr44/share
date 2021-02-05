4c30fa9d714eb7b83c7d3e3cecb045c7e0cb2bcf
Abhinav Kumar <65457668+abhinav-workspace@users.noreply.github.com>
2021-02-05 10:09:44 +0530

    Update treealgorithm.java

--- treealgorithm.java
+++ treealgorithm.java
@@ -33,6 +33,10 @@ public class Test {
             }
         }
     }
+    
+    
+    
+    
 
     class Node {
         Object id;
@@ -67,6 +71,33 @@ public class Test {
         }
 
     }
-
-
 }
+
+public class TreePaths {
+    private static List<List<TreeNode>> getPaths0(TreeNode pos) {
+        List<List<TreeNode>> retLists = new ArrayList<>();
+
+        if(pos.getChildren().size() == 0) {
+            List<TreeNode> leafList = new LinkedList<>();
+            leafList.add(pos);
+            retLists.add(leafList);
+        } else {
+            for (TreeNode node : pos.getChildren()) {
+                List<List<TreeNode>> nodeLists = getPaths0(node);
+                for (List<TreeNode> nodeList : nodeLists) {
+                    nodeList.add(0, pos);
+                    retLists.add(nodeList);
+                }
+            }
+        }
+
+        return retLists;
+    }
+
+    public static List<List<TreeNode>> getPaths(TreeNode head) {
+        if(head == null) {
+            return new ArrayList<>();
+        } else {
+            return getPaths0(head);
+        }
+    }
